9b892272194a68aecb14dffed412da534414f088
Sam1ndaFernando <uthpala2001@gmail.com>
2024-02-17 21:32:29 +0530

    Wish page added, automatically redirects to another page.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+/*
 var target_mili_sec = new Date("March 07, 2024 00:00:00").getTime();
+*/
+var target_mili_sec = new Date("Feb 17, 2024 21:31:00").getTime();
 function timer() {
     var now_mili_sec = new Date().getTime();
-    var remaining_sec = Math.floor( (target_mili_sec - now_mili_sec) / 1000 );
+    var remaining_sec = Math.floor((target_mili_sec - now_mili_sec) / 1000);
 
     var day = Math.floor(remaining_sec / (3600 * 24));
     var hour = Math.floor((remaining_sec % (3600 * 24)) / 3600);
@@ -12,6 +15,12 @@ function timer() {
     document.querySelector("#hour").innerHTML = hour;
     document.querySelector("#min").innerHTML = min;
     document.querySelector("#sec").innerHTML = sec;
+
+    if (remaining_sec <= 0) {
+        clearInterval(interval); // Stop the timer
+        // Redirect to another HTML file
+        window.location.replace("wish.html");
+    }
 }
 
-setInterval(timer, 1000); //1000 it means 1 sec
+var interval = setInterval(timer, 1000); //1000 ms = 1 sec
