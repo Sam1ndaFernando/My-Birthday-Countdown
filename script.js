8adf3eb3e493ee303a3e29005997dc082cd0740e
Sam1ndaFernando <uthpala2001@gmail.com>
2024-02-18 23:13:43 +0530

    js file added and bugs fix

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-/*
 var target_mili_sec = new Date("March 07, 2024 00:00:00").getTime();
-*/
-var target_mili_sec = new Date("Feb 17, 2024 21:31:00").getTime();
+// var target_mili_sec = new Date("Feb 17, 2024 21:31:00").getTime();
 function timer() {
     var now_mili_sec = new Date().getTime();
     var remaining_sec = Math.floor((target_mili_sec - now_mili_sec) / 1000);
